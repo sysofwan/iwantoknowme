77b3320dd8eae8347645a6cad1bdda376c3b5674
Sofwan Haddad <sysofwan@umich.edu>
2015-10-17 08:00:47 -0500

    fixed bunch of bugs

--- data-collector.js
+++ data-collector.js
@@ -4,44 +4,75 @@
 
 	var currentTab;
 	var currentTabStartTime;
-	var tabViews = [];
 
 	var saveTab = function() {
+		console.log('save tab called');
 		if (currentTab) {
 			data = {
-				title:currentTab.title,
-				url:currentTab.url,
-				startDate:currentTabStartTime,
-				endDate:new Date()
+				title: currentTab.title,
+				url: currentTab.url,
+				domain: util.urlParser(currentTab.url).hostname,
+				startDate: currentTabStartTime,
+				endDate: new Date()
 			};
-			tabViews.push(data);
+			tabsDB.add(data);
+			console.log(data);
+		}
+	};
+
+	var changeCurrentTab = function(tab) {
+		saveTab();
+		currentTab = null;
+		currentTabStartTime = null;
+		if (tab) {
+			var protocol = util.urlParser(tab.url).protocol;
+			if (protocol != 'chrome:') {
+				currentTab = tab;
+				currentTabStartTime = new Date();
+			}
 		}
 	};
 
 	tabsLib.onActivated.addListener(function(activeInfo) {
+		console.log('onActivated called');
 		tabsLib.get(activeInfo.tabId, function(tab) {
-			saveTab();
-			currentTab = tab;
-			currentTabStartTime = new Date();
+			if (!currentTab || currentTab.id != tab.id) {
+				changeCurrentTab(tab);
+			}
 		});
-		if(tabViews.length != 0){
-			console.log(tabViews[0]["endDate"])
+	});
+
+	tabsLib.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		console.log('onUpdated called');
+		if (changeInfo.url && (!currentTab || (currentTab.url != changeInfo.url))) {
+			changeCurrentTab(tab);
 		}
 	});
 
 	windows.onFocusChanged.addListener(function(windowId) {
-		saveTab();
-		currentTab = null;
-		currentTabStartTime = null;
-		if (windowId != windows.WINDOW_ID_NONE) {
+		console.log('onFocusChanged called');
+		if (windowId == windows.WINDOW_ID_NONE) {
+			changeCurrentTab(null);
+		}
+		else {
 			windows.get(windowId, function(window) {
-				if (window.type == "normal") {
-					tabsLib.query({active: true, windowId: windowId}, function(tab) {
-						currentTab = tab[0];
-						currentTabStartTime = new Date();
+				if (window.type != "normal") {
+					changeCurrentTab(null);
+				} else {
+					tabsLib.query({active: true, windowId: windowId}, function(tabs) {
+						var tab = tabs[0];
+						if (!currentTab || tab.id != currentTab.id) {
+							changeCurrentTab(tab[0]);
+						}
 					});
 				}
 			});
 		}
 	});
+
+	setInterval(function() {
+		tabsDB.getAll(function(res) {
+			console.log(res);
+		});
+	}, 100000);
 }());
