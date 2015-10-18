d281542b91f302388b79389551e9d3f9a13a344f
Sofwan Haddad <sysofwan@umich.edu>
2015-10-18 05:38:25 -0500

    fixed unknown error

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
-(function() {
+$(function() {
 	$('.btn-footer button').on('click', function() {
 		chrome.tabs.create({ url: "browsinghabit.html" });
 	});
 
 	var duration = function(d) {return d.endDate - d.startDate;};
+	var dial = $(".dial").knob();
 
 	var yest = new Date()
 	yest.setDate(yest.getDate() - 1);
@@ -11,8 +12,7 @@
 		var group = crossfilter.groupAll();
 		var g = group.reduceSum(duration);
 		var dial = $(".dial");
-		dial.val(Math.ceil(g.value()/(1000 * 60 * 60)));
-		dial.knob();
+		dial.val(Math.ceil(g.value()/(1000 * 60 * 60))).trigger('change');
 
 		var dimen = crossfilter.getDomainDimension();
 		var domainGroup = dimen.group();
@@ -23,6 +23,5 @@
 		groupedVal.forEach(function(obj) {
 			topSitesList.append('<li>' + obj.key + '</li>');
 		});
-
 	});
-}());
+});
