dcc1798f6e5ea3d3dfa39da7b4212c12442eb9be
mzwee <mzwee95@gmail.com>
2015-10-18 00:33:35 -0400

    made things look prettier

--- browsinghabit.js
+++ browsinghabit.js
@@ -2,7 +2,7 @@
 
 var dimHelper = function(dim) {
   return dim.group().reduceSum(function(d) {
-    return (d.endDate - d.startDate);
+    return (d.endDate - d.startDate) / 60000;
   });
 };
 
@@ -12,11 +12,12 @@ tabsXFilter.getAllDataXFilter(function(crossFtr) {
 
   var durationGroup = dimHelper(dim);
 
-  yearChart.width(990) 
+  yearChart.width(1750) 
           .height(50)
           .margins({top: 0, right: 50, bottom: 20, left: 40})
           .dimension(dim)
           .group(durationGroup)
+          .colors(['#f27863'])
           .centerBar(true)
           .gap(1)
           .x(d3.time.scale().domain([new Date(2015, 9, 17, 14, 0, 0), new Date(2015, 9, 17, 15, 0, 0)]))
@@ -25,7 +26,7 @@ tabsXFilter.getAllDataXFilter(function(crossFtr) {
 
   var yearLineChart = dc.lineChart('#year-line-chart');
   yearLineChart.renderArea(true)
-              .width(990)
+              .width(1750)
               .height(200)
               .transitionDuration(1000)
               .margins({top: 30, right: 50, bottom: 25, left: 40})
@@ -37,46 +38,41 @@ tabsXFilter.getAllDataXFilter(function(crossFtr) {
               .xUnits(d3.time.minutes)
               .elasticY(true)
               .renderHorizontalGridLines(true)
-              .group(durationGroup);
+              .group(durationGroup)
+              .colors(['#f27863']);
 
   var domainChart = dc.rowChart('#domain-chart');
   var domainDim = crossFtr.getDomainDimension();
   var domainDurationGroup = dimHelper(domainDim);
-  domainChart.width(300)
-            .height(230)
+  domainChart.width(700)
+            .height(275)
             .margins({top: 20, left: 10, right: 10, bottom: 20})
             .dimension(domainDim)
             .group(domainDurationGroup)
-            .colors(['#acdfd2', '#dcda98', '#f29885', '#c6dbef', '#dadaeb'])
-            .label(function (d) {
-                return d.key.split('.')[1];
-            })
+            .colors(['#a4a8a4', '#acdfd2', '#dcda98', '#f29885', '#a4a8a4', '#acdfd2', '#dcda98', '#f29885', '#a4a8a4', '#acdfd2', '#dcda98', '#f29885'])
             .title(function (d) {
                 return d.value;
             })
             .elasticX(true)
             .xAxis().ticks(4);
+  console.log(domainDurationGroup.top(5));
 
   var hourIntChart = dc.barChart('#hour-int-chart');
   var hourIntDim = crossFtr.getHourIntDimension();
   var durationIntGroup = dimHelper(hourIntDim);
   hourIntChart
-    .width(420)
-    .height(180)
-    .margins({top: 10, right: 50, bottom: 30, left: 40})
+    .width(700)
+    .height(275)
+    .margins({top: 10, right: 50, bottom: 20, left: 40})
     .dimension(hourIntDim)
     .group(durationIntGroup)
+    .colors(['#f27a63'])
     .elasticY(true) 
     .centerBar(true) 
     .gap(1) 
     .round(dc.round.floor)
     .x(d3.scale.linear().domain([0, 24]))
-    .renderHorizontalGridLines(true) 
-    .filterPrinter(function (filters) {
-        var filter = filters[0], s = '';
-        s += numberFormat(filter[0]) + '% -> ' + numberFormat(filter[1]) + '%';
-        return s;
-    });
+    .renderHorizontalGridLines(true);
  
   hourIntChart.xAxis().tickFormat(
       function (v) { return v; });
