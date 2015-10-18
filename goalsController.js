6b92e8aa195abf620fe02141b6d99be9b171c4bd
Yusuf Azman <stylo@umich.edu>
2015-10-18 00:55:48 -0400

    added storage to goal setting

--- goalsController.js
+++ goalsController.js
@@ -3,19 +3,24 @@ var browsingAnalyticsApp = angular.module('browsingAnalyticsApp', []);
 browsingAnalyticsApp.controller("GoalsController", function($scope) {
 	
 	$scope.init = function() {
-		if (localStorage["goals"]==null){
-			localStorage["goals"]=[];
-		}
-		$scope.goals = localStorage;
-		
+		chrome.storage.local.get("goals", function(result) {
+			if(_.isEmpty(result)) {
+				chrome.storage.local.set({"goals": []});
+				$scope.goals = [];
+			}
+			else {
+				console.log(result);
+				$scope.goals = result;
+			}
+		});
 	}
 
 	$scope.addGoal = function() {
 		var goal = {
-			domain: $scope.domain;
-			duration: $scope.duration;
+			domain: $scope.domain,
+			duration: $scope.duration
 		}
 		$scope.goals.push(goal);
-		localStorage["goals"].push(goal);
+		chrome.storage.local.set({"goals": $scope.goals});
 	};
 });
