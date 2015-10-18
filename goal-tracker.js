824da1013a1955b3ec9c3d2a8623f8a3769a5893
Yusuf Azman <stylo@umich.edu>
2015-10-18 05:36:58 -0400

    changed to chrome notifications

--- goal-tracker.js
+++ goal-tracker.js
@@ -1,18 +1,35 @@
 (function() {
+	var d = new Date();
+	var prevDate = d.getDate();
 	setInterval(function() {
 		var goals = JSON.parse(localStorage["goals"]);
-		var startTime = new Date();
 		var endTime = new Date();
-		startTime.setTime(startTime.getTime() - (24 * 60 * 60 * 1000));
+		var startTime = new Date(endTime.getYear(), endTime.getMonth(), endTime.getDate());
+		var date = endTime.getDate();
+		if(prevDate != date) {
+			for(var goal of goals) {
+				goal.enable = true;
+			}
+		}
 		tabsDB.filterRange("startDate", startTime, endTime, function(result) {
 			var selectedTabs = result;
+			console.log(selectedTabs);
 			for(var goal of goals) {
 				var tabsByDomain = _.filter(selectedTabs, { "domain": goal.domain });
 				var minutesSpent = _.sum(tabsByDomain, function(elem) {
 					return (elem.endDate - elem.startDate) / 1000 / 60;
 				});
 				if(minutesSpent >= goal.duration) {
-					alert("exceed limit");
+					chrome.notifications.clear(goal.domain);
+					var notification = chrome.notifications.create(
+						goal.domain,
+						{ 
+							type: "basic",
+							iconUrl: "angry.png",
+							title: goal.domain + " overload notice",
+							message: "Remember the project due next Monday?"
+						}
+					);
 				}
 			}
 		});
